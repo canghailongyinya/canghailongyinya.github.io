90d8dd9543f05ced4d31da912ce7e6f994dc154d
Strange-Hai <longyh27@mail2.sysu.edu.cn>
2025-01-11 15:25:20 +0800

    Site updated: 2025-01-11 15:25:20

---
+++ js/random-bg.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function() {
+    // 定义本地图片路径
+    const images = [
+      '/images/bg1.jpeg',
+      '/images/bg2.jpeg',
+      '/images/bg3.jpeg'
+    ];
+  
+    // 随机选择一张图片
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+  
+    // 设置背景图片
+    document.body.style.backgroundImage = `url(${randomImage})`;
+  
+    // 如果你想设置 footer 背景
+    const footer = document.querySelector('footer');
+    if (footer) {
+      footer.style.backgroundImage = `url(${randomImage})`;
+    }
+  });
+  
