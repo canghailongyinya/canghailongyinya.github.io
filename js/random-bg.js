2c7276e13b5c1a66cc12634475ef0b8687ec0771
Strange-Hai <longyh27@mail2.sysu.edu.cn>
2025-01-11 16:13:28 +0800

    Site updated: 2025-01-11 16:13:27

--- js/random-bg.js
+++ js/random-bg.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function() {
     // 定义本地图片路径
     const images = [
-      '/images/bg1.jpeg',
-      '/images/bg2.jpeg',
-      '/images/bg3.jpeg'
+      '/images/top_img1.jpeg',
+      '/images/top_img2.jpeg',
+      '/images/top_img3.jpeg'
     ];
   
     // 随机选择一张图片
